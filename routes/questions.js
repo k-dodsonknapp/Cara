189a5be66e182d80c49ce3fdf1ea37ce89140323
Eric-Cortez <ecrcortez@gmail.com>
2021-12-14 10:35:53 -0800

    get / route tested

--- routes/questions.js
+++ routes/questions.js
@@ -1,20 +1,23 @@
 const express = require('express');
+const router = express.Router();
+
+const { csrfProtection, asyncHandler } = require("./utils");
 const { check, validationResult } = require("express-validator");
+const { loginUser, restoreUser, logoutUser, requireAuth } = require("../auth")
 
 const db = require("../db/models");
 const { Question } = db
-const { csrfProtection, asyncHandler } = require("./utils");
-
-const router = express.Router();
-
 
 //GET TEN COMMENTS - (BONUS order them by popularity)
-
 router.get( "/", asyncHandler(async (req, res) => {
     const questions = await Question.findAll({ limit: 10 });
     res.send("testing question / route ")
     // res.render("question-detail", { title: "Questions", questions });
   })
 );
+
+
+
+
 
 module.exports = router
